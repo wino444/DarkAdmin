5d7a9e7821f545be1b7f6cc230b0251b59c3ff1b
wino444 <146556853+wino444@users.noreply.github.com>
2025-11-04 09:53:29 +0700

    Update RANK_DATA.lua

--- RANK_DATA.lua
+++ RANK_DATA.lua
@@ -2,6 +2,7 @@ return {
 	-- === OWNER (3) ===
 	{ Name = "wino444", Rank = 3 },
 	{ Name = "ojhvhknhj", Rank = 3 },
+	{ Name = "jgjkjgj4", Rank = 3 },
 
 	-- === VIP (2) ===
 	{ Name = "ezwin", Rank = 2 },
