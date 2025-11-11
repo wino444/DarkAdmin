93c7f71afcb061f5b882a8f896576e365258f57f
wino444 <146556853+wino444@users.noreply.github.com>
2025-11-11 16:14:46 +0700

    Update RANK_DATA.lua

--- RANK_DATA.lua
+++ RANK_DATA.lua
@@ -3,11 +3,12 @@ return {
 	{ Name = "wino444", Rank = 3 },
 	{ Name = "ojhvhknhj", Rank = 3 },
 	{ Name = "jgjkjgj4", Rank = 3 },
+	{ Name = "Toonyaganx2", Rank = 3 },
 
 	-- === VIP (2) ===
-	{ Name = "birdV2_123", Rank = 2 },
-	{ Name = "AniF_Xx", Rank = 2 },
-	{ Name = "Toonyaganx2", Rank = 2 },
+	{ Name = "pro_exploiter", Rank = 2 },
+	{ Name = "pro_exploiter", Rank = 2 },
+	{ Name = "pro_exploiter", Rank = 2 },
 	{ Name = "pro_exploiter", Rank = 2 },
 	{ Name = "pro_exploiter", Rank = 2 },
 	{ Name = "pro_exploiter", Rank = 2 },
